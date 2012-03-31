73a3e8c444b0a4f2da0be2ab28dc459775c3882f
Clust3r <ghys.jeremy@gmail.com>
2012-03-31 09:17:02 +0200

    hop bacon to clusty

--- envsetup.sh
+++ envsetup.sh
@@ -437,7 +437,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka clusty
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
